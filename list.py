47c4ae962f09243514eb0ee7ccd5116768e165f4
Geetha <geetha.notify@gmail.com>
2022-06-03 08:10:13 +0100

    Delete list.py

--- list.py
+++
@@ -1,6 +0,0 @@
-performances = {'Ventriloquism':'9:00am', 
-                'Snake Charmer': '12:00pm', 
-                'Amazing Acrobatics': '2:00pm', 
-                'Enchanted Elephants':'5:00pm'}
-for i,j in performances.items():
-    print(i,':',j)
