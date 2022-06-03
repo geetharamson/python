dfcf7e6f56cd67d0c201ea5657ef432db16abb1b
Geetha <geetha.notify@gmail.com>
2022-06-03 08:08:34 +0100

    Add files via upload

---
+++ list.py
@@ -0,0 +1,6 @@
+performances = {'Ventriloquism':'9:00am', 
+                'Snake Charmer': '12:00pm', 
+                'Amazing Acrobatics': '2:00pm', 
+                'Enchanted Elephants':'5:00pm'}
+for i,j in performances.items():
+    print(i,':',j)
